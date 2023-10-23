7e4c42b6305ee3716e0edd518e1f4937e6e9e83d
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-23 13:58:16 +0300

    updated skills

--- src/Components/Pages.js
+++ src/Components/Pages.js
@@ -1,56 +1,29 @@
-import About from './About'
-import './css/Pages.css'
-import Picture from './Picture'
-import Card from '../Components/Card';
-import SkillsButton from '../Components/SkillsButton';
-import Footer from './Footer';
-import Slider from './Slider';
-
-
-
+import About from "./About";
+import "./css/Pages.css";
+import Picture from "./Picture";
+import Card from "../Components/Card";
+import SkillsButton from "../Components/SkillsButton";
+import Footer from "./Footer";
+import Slider from "./Slider";
 
 export default function Pages() {
+  return (
+    <div className="main-html-div">
+      <div className="html-div">
+        <Picture />
+        <About />
+        <Footer />
+      </div>
 
-
-
-
-    return (
-
-
-
-        <div className="main-html-div">
-            
-  
-            <div className="html-div">
-
-
-                <Picture />
-                <About />
-                <Footer />
-
-
-            </div>
-
-            <div className="html2-div">
-
-            <div className="html2-2component">
-          
-                <Card />
-                <SkillsButton />
-                </div>
-
-                <Slider />
-                <Footer />
-                
-
-            </div>
-
-
-
-
+      <div className="html2-div">
+        <div className="html2-2component">
+          <Card />
+          <SkillsButton />
         </div>
 
-    )
-
-
+        <Slider />
+        <Footer />
+      </div>
+    </div>
+  );
 }
