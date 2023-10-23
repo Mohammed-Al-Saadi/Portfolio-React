a047f02835c11f5056d95a9604f59cc808c78d08
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-23 13:21:08 +0300

    updated skills

--- src/Components/data.js
+++ src/Components/data.js
@@ -1,55 +1,32 @@
+export const skills = [
+  {
+    id: 1,
 
+    body: ["HTML", "CSS", "Java", "JavaScripts", "React js", "React native"],
+  },
 
- 
-  
+  {
+    id: 2,
 
+    body: ["PHP", "Databases", "RestAPI", "Java", "Node js"],
+  },
+  {
+    id: 3,
 
-
-
-
-export const skills = [{
-id:1,
-  
-body: ['HTML',
-  'CSS',
-  'Java',
-  'JavaScripts',
-   'React js' ]
-
-
-},
-
-{
-id:2,
-
-body: ['PHP',
-'MySQL',
-'RestAPI',
-'Java(servlet, JSP)',
- ]
-}
-,
-{
-id:3,
-
-body: ['OOP',
-'Docker',
-'Robot framework',
-'Machine learning',
-'Git Version Control',
-'Agile Methodologies',
-'Robotic process automation',
-
- ]
-},
-{
-
-id:4,
-body: ['Häme University of applied sciences(Computer Applications) Second year',
-  'Al-Yarmouk University CollegeBachelor of Computer EngineeringCompleted in 2014',
-  
-   ]
-
-}
-
-]
+    body: [
+      "Docker",
+      "Robot framework",
+      "Machine learning",
+      "Git Version Control",
+      "Agile Methodologies",
+      "Robotic process automation",
+    ],
+  },
+  {
+    id: 4,
+    body: [
+      "Häme University of applied sciences(Computer Applications) Second year",
+      "Al-Yarmouk University CollegeBachelor of Computer EngineeringCompleted in 2014",
+    ],
+  },
+];
