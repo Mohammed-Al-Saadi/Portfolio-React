72893b3fd628ac59067f52419d0b8be51a0f9206
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-10-23 13:35:30 +0300

    updated skills

--- src/Components/Darkmode.js
+++ src/Components/Darkmode.js
@@ -1,32 +1,24 @@
 import React, { useEffect, useState } from "react";
-import './css/Darkmood.css';
-
+import "./css/Darkmood.css";
 
 export default function Darkmode() {
-    const [theme, setTheme] = useState(false);
+  const [theme, setTheme] = useState(true);
 
-    const handleClick = () => {
-        setTheme(!theme)
+  const handleClick = () => {
+    setTheme(!theme);
+  };
+  useEffect(() => {
+    if (theme === true) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
     }
-    useEffect(() => {
-        if (theme === true) {
-            document.body.classList.add("dark");
+  });
 
-        } else {
-            document.body.classList.remove("dark");
-        }
-    })
-
-
-
-
-    return (
-        <div className="theme">
-
-            <input onClick={handleClick} type="checkbox" id="switch" /><label className="toggle" htmlFor="switch"></label>
-
-        </div>
-    );
-
-
-};
+  return (
+    <div className="theme">
+      <input onClick={handleClick} type="checkbox" id="switch" />
+      <label className="toggle" htmlFor="switch"></label>
+    </div>
+  );
+}
