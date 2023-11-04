f5759b49b988b2d479a7aabd2912cf08c299a9b5
Mohammedalihussien <89648227+Mohammedalihussien@users.noreply.github.com>
2023-11-04 14:44:03 +0200

    added link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import {   BsLinkedin } from "react-icons/bs"
-import {   GrInstagram,GrFacebook } from "react-icons/gr"
-import {   AiFillGithub } from "react-icons/ai"
-import './css/Footer.css'
-
+import React from "react";
+import { BsLinkedin } from "react-icons/bs";
+import { GrInstagram, GrFacebook } from "react-icons/gr";
+import { AiFillGithub } from "react-icons/ai";
+import "./css/Footer.css";
 
 export default function Footer() {
-
-    return (
-        <div className='profile-container'>
-
-            <div className="profile-details">
-
-                <div className="colz">
-                    <a href='https://www.facebook.com/profile.php?id=100001946923572'>
-                    <i><GrFacebook size={20}/></i>
-                    </a>
-                    <a href='https://www.instagram.com/mohammed.ali9o/?next=%2F'>
-                    <i><GrInstagram size={20}/></i>
-                    </a>
-                    <a href='https://www.linkedin.com/in/mohammed-al-saadi-08a502264/'>
-                    <i><BsLinkedin size={20}/></i>
-                    </a>
-                    <a href='https://github.com/Mohammed-Al-Saadi'>
-                        <i><AiFillGithub size={20}/></i>
-                    </a>
-
-                </div>
-                <div className="rights">
-
-                    <p>Mohammed Al-Saadi © 2023 All Rights Reserved </p>
-
-                </div>
-
-
-
-            </div>
-
+  return (
+    <div className="profile-container">
+      <div className="profile-details">
+        <div className="colz">
+          <a href="https://www.facebook.com/profile.php?id=100001946923572">
+            <i>
+              <GrFacebook size={20} />
+            </i>
+          </a>
+          <a href="https://www.instagram.com/mohammed.ali9o/?next=%2F">
+            <i>
+              <GrInstagram size={20} />
+            </i>
+          </a>
+          <a href="https://www.linkedin.com/in/mohammed-al-saadi-08a502264/">
+            <i>
+              <BsLinkedin size={20} />
+            </i>
+          </a>
+          <a href="https://github.com/Mohammed-Al-Saadi">
+            <i>
+              <AiFillGithub size={20} />
+            </i>
+          </a>
+          <a href="https://github.com/Mohammed-Al-Saadi">
+            <i>
+              <AiFillGithub size={20} />
+            </i>
+          </a>
         </div>
-
-    )
+        <div className="rights">
+          <p>Mohammed Al-Saadi © 2023 All Rights Reserved </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-
