36c303c7409c60859202fa8ff397047d2539c838
Mohammed <al-saadi.mohammed@outlook.com>
2024-10-17 20:11:53 +0300

    Refactor: Restructure project directory for improved organization

--- src/Components/contactComponent/emailUtils.js
+++ src/Components/contactComponent/emailUtils.js
@@ -16,10 +16,10 @@ export const sendEmail = (form, setLoading, setSuccessMessage) => {
 
   emailjs
     .sendForm(
-      "service_zy1oqvf",
-      "template_rbmt8va",
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
       form.current,
-      "YwJIiLEPe-Xm3FKSC"
+      process.env.REACT_APP_EMAILJS_USER_ID
     )
     .then(
       (result) => {
