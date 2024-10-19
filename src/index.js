3f174f521e2ab55a0ed1ba4384be3e5da5fd2227
Mohammed <al-saadi.mohammed@outlook.com>
2024-10-19 13:21:26 +0300

    added google analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
+import { useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-5N7LWRCKWH");
+
+function AnalyticsWrapper() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname,
+      title: document.title,
+    });
+  }, [location]);
+
+  return null;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      {/* Wrap your app in AnalyticsWrapper to track pageviews */}
+      <AnalyticsWrapper />
       <App />
     </BrowserRouter>
   </React.StrictMode>
