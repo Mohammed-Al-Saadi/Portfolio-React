85102eadd1000d2a5de0f5cba8ba3c4acc98c442
Mohammed <al-saadi.mohammed@outlook.com>
2024-10-20 23:42:38 +0300

    added GPA

--- src/Components/educationComponent/education.js
+++ src/Components/educationComponent/education.js
@@ -3,6 +3,7 @@ import "./education.css";
 import { BsCheckCircle } from "react-icons/bs";
 import { FaUniversity } from "react-icons/fa";
 import { EducationData } from "./educationData.js";
+import { FaUserGraduate } from "react-icons/fa6";
 
 export default function Education() {
   return (
@@ -16,11 +17,15 @@ export default function Education() {
                 <FaUniversity color="black" size={20} className="icon" />{" "}
                 <span>{item.name}</span>
               </div>
-
               {/* Completion year */}
               <div className="education-completion-year">
-                <BsCheckCircle size={18} color="green" className="icon" />
+                <FaUserGraduate size={18} color="black" className="icon" />
                 <span>Completed in {item.completed}</span>
+              </div>
+              {/* GPA */}
+              <div className="education-gpa">
+                <BsCheckCircle size={18} color="green" className="icon" />
+                <span>GPA {item.gpa}</span>
               </div>
             </div>
           </div>
--- src/Components/educationComponent/educationData.js
+++ src/Components/educationComponent/educationData.js
@@ -4,14 +4,16 @@ import yarmoukUniversityImage from "../../assets/images/logo_yuc.png";
 export const EducationData = [
   {
     name: "Häme University of Applied Sciences (Computer Applications)",
-    image: hameUniversityImage, 
+    image: hameUniversityImage,
     completed: "2024",
+    gpa: "4.3/5",
     logoClassName: "education-card-image-hame",
   },
   {
     name: "Al-Yarmouk University College (Computer Engineering)",
-    image: yarmoukUniversityImage, 
+    image: yarmoukUniversityImage,
     completed: "2014",
+    gpa: "61/100",
     logoClassName: "education-card-image-yarmouk",
   },
 ];
